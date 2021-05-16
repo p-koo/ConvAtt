ce7cfbce9ad202d643c8745dec78961e99376fa1
koo-lab <koolaboratory.gmail.com>
2021-05-16 09:15:04 -0400

    updates

--- invivo_pipeline.py
+++ invivo_pipeline.py
@@ -3,6 +3,7 @@ import requests as rq
 import io, h5py, os
 from six.moves import cPickle
 
+import tfomics
 import tensorflow as tf
 from tfomics import moana, evaluate
 import models
@@ -174,8 +175,8 @@ jaspar_dir = 'motif_database.txt'
 output = moana.tomtom(motif_dir, jaspar_dir, tomtom_dir, evalue=False, thresh=0.5, dist='pearson', png=None, tomtom_path='tomtom')
 
 # motif analysis
-num_filters = count_meme_entries(motif_dir)
-stats = tfomics.evaluate(os.path.join(tomtom_dir,'tomtom.tsv'), num_filters)
+num_filters = moana.count_meme_entries(motif_dir)
+stats = tfomics.evaluate.motif_comparison_synthetic_dataset(os.path.join(tomtom_dir,'tomtom.tsv'), num_filters)
 stats_dir = os.path.join(results_path, model_name+'_stats_'+str(trial)+'.npy')
 np.save(stats_dir, stats, allow_pickle=True)
 
